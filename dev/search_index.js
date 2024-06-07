102a2b08bd64cf849e8d9983b628d32f023029ab
Documenter.jl <documenter@juliadocs.github.io>
2024-06-07 12:28:38 +0000

    build based on 212bd94

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = VDJDL","category":"page"},{"location":"#VDJDL","page":"Home","title":"VDJDL","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for VDJDL.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [VDJDL]","category":"page"}]
+}
